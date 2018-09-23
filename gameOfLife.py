f48d998254c864534c1d1032c0f4e88dc074a6df
RohithS98 <ee16b025@iittp.ac.in>
2018-09-23 21:46:48 +0530

    Added next step and consol print

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,4 +1,12 @@
 from random import randint
+from time import sleep
+import os
+
+def clear():
+    if os.name == 'nt': 
+        os.system('cls') 
+    else: 
+        os.system('clear') 
 
 class Board:
 
@@ -17,5 +25,45 @@ class Board:
             st += '\n'
         return st
 
+    def update(self):
+        self.gameBoard = Board.nextState(self.gameBoard)
+
+    @staticmethod
+    def neighbourCount(board,r,c):
+        count = 0
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if (0 <= i+r < len(board)) and (0 <= j+c < len(board[0])) and not(i == 0 and j == 0):
+                    if board[i+r][j+c] == 1:
+                        count+=1
+        return count
+
+    @staticmethod
+    def nextState(board):
+        newBoard = [[0 for i in range(len(board[0]))] for j in range(len(board))]
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                temp = Board.neighbourCount(board,i,j)
+                if temp < 2 or temp > 3:
+                    newBoard[i][j] = 0
+                elif temp == 3:
+                    newBoard[i][j] = 1
+                else:
+                    newBoard[i][j] = board[i][j]
+        return newBoard
+
+def test(ans,exp):
+    for i in range(len(exp)):
+        for j in range(len(exp[0])):
+            if ans[i][j] != exp[i][j]:
+                return False
+    return True
+
+
 life = Board()
-print(life)
+for i in range(2):
+    clear()
+    print(life)
+    life.update()
+    sleep(0.25)
+x = input('Press Enter to exit')
